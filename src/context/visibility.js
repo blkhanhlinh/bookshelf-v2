628ce2ca21506aecb583ae305adc4ab10223b6f9
blkhanhlinh <linhbui8103@gmail.com>
2024-04-19 17:45:10 +0700

    add hook to control the visible of the header when a book is added to cart 👀

---
+++ src/context/visibility.js
@@ -0,0 +1,14 @@
+import { createContext, useContext, useState } from "react";
+
+const VisibilityContext = createContext();
+
+export const useVisibility = () => useContext(VisibilityContext);
+
+export const VisibilityProvider = ({ children }) => {
+    const [isVisible, setIsVisible] = useState(true);
+    return (
+        <VisibilityContext.Provider value={{ isVisible, setIsVisible }}>
+            {children}
+        </VisibilityContext.Provider>
+    )
+}
