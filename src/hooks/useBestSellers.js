d720fcb5a1ad65cdd4f0946e34f742199e845ca1
blkhanhlinh <linhbui8103@gmail.com>
2024-04-19 16:46:03 +0700

    Add hooks to virtually filter best seller and new arrival books

---
+++ src/hooks/useBestSellers.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const useBestSellers = (books) => {
+    const [bestSellers, setBestSellers] = useState([]);
+
+    useEffect(() => {
+        if (books.length > 0) {
+            const bestSellers = [...books].sort((a, b) => {
+                return b.average_rating - a.average_rating;
+            }).slice(0, 20);
+            setBestSellers(bestSellers);
+        }
+    }, [books]);
+
+    return bestSellers;
+};
+
+export default useBestSellers;
---
+++ src/hooks/useNewArrivals.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const useNewArrivals = (books) => {
+    const [newArrivals, setNewArrivals] = useState([]);
+
+    useEffect(() => {
+        if (books.length > 0) {
+            const sortedBooks = [...books].sort((a, b) => {
+                return b.published_year - a.published_year; 
+            }).slice(0, 20);
+            setNewArrivals(sortedBooks);
+        }
+    }, [books]);
+
+    return newArrivals;
+}
+
+export default useNewArrivals;
